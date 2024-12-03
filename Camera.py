ade5d08ede52589efdc5ba96f7b7c5adba5c97e5
NJUyd2001 <958902573@qq.com>
2024-12-03 14:13:40 +0800

    增加拍摄图片显示

--- Camera.py
+++ Camera.py
@@ -1,7 +1,8 @@
 import salabim as sim
 import enum
 import random
-
+from PIL import Image   
+from typing import TypedDict
 #摄像头的状态持续时间
 CAMERA_ACTIVE_TIME=20
 CAMERA_SLEEP_TIME=50
@@ -9,7 +10,8 @@ CAMERA_POS_Y_OFFSET=10
 
 #服务器位置
 SERVER_POS_Y_OFFSET=30
-
+#服务器到painter的距离长度
+SERVER_TO_PAINTER_PATH=200
 # 照片的不同状态，暂时废除
 # class DeviceState(enum.Enum):
 #     Error = enum.auto()  
@@ -58,7 +60,7 @@ def do_animation():
     cams = []
     
     #通过设置的尺寸和容量得到初始生成点的位置信息
-    x = env.width()-serverCapacity*xvisitor_dim
+    x = env.width()-serverCapacity*xvisitor_dim-SERVER_TO_PAINTER_PATH-painterCapacity*xvisitor_dim
     x -= (pipeCapacity + 5) * xvisitor_dim #减去的值等于信道长度
     xsign = x #显示编号的位置
     x -= xvisitor_dim / 2
@@ -92,12 +94,23 @@ def do_animation():
 
     #对服务器的位置进行计算和绘制
     for Server in Servers:
-        x = env.width()-xvisitor_dim*serverCapacity
+        offset=xvisitor_dim*serverCapacity+xvisitor_dim*painterCapacity+SERVER_TO_PAINTER_PATH
+        x = env.width()-offset
         y = ypath0 + Server.n * yvisitor_dim*1.5
         Server.pip = sim.AnimateRectangle(
             x=x, y=y+SERVER_POS_Y_OFFSET, spec=(0, 0, serverCapacity * xvisitor_dim, yvisitor_dim), fillcolor="gray", linewidth=0
         )
         sim.AnimateQueue(queue=Server.visitors, x=x, y=y+SERVER_POS_Y_OFFSET, direction="e", title="", arg=Pipe,reverse=True)
+    nPipes_last = nPipes
+
+    #对Painter的位置进行计算和绘制
+    for Painter in Painters:
+        x = env.width()-xvisitor_dim*painterCapacity
+        y = ypath0 + Painter.n * yvisitor_dim*1.5
+        Painter.pip = sim.AnimateRectangle(
+            x=x, y=y+SERVER_POS_Y_OFFSET, spec=(0, 0, painterCapacity * xvisitor_dim, yvisitor_dim), fillcolor="gray", linewidth=0
+        )
+        sim.AnimateQueue(queue=Painter.pictures, x=x, y=y+SERVER_POS_Y_OFFSET, direction="e", title="", arg=Pipe,reverse=True)
     nPipes_last = nPipes
     env.animate(True)
 
@@ -267,8 +280,14 @@ class VisitorGenerator(sim.Component):
 #             return DeviceState.Warning
 #         else :
 #             return state
+
+class ImageInput(TypedDict):
+    path: str
+    
 class Visitor(sim.Component):
     def setup(self, from_, to):
+        self.state = 2
+        self.img = Image.open("test.jpg")
         self.frompath = paths[from_]
         #输入路径的当前数据包数量
         paths[from_].vnum +=  1
@@ -284,15 +303,25 @@ class Visitor(sim.Component):
         size_x = xvisitor_dim
         size_y = yvisitor_dim
         b = 0.1 * xvisitor_dim
-        an0 = sim.AnimateRectangle(
-            spec=(b, 2, xvisitor_dim - b, yvisitor_dim - b),
-            linewidth=0,
-            fillcolor="Green",
-            # text=str(self.topath.n),
-            text=str(self.num),
-            fontsize=xvisitor_dim * 0.7,
-            textcolor="white",
-        )
+        img=Image.open("test.jpg")
+        if self.state == 1:
+            an0 = sim .AnimateImage(
+                image=img,
+                x=xvisitor_dim-b,
+                y=yvisitor_dim-b,
+                width=50,
+                height=50,
+            )
+        else:
+            an0 = sim.AnimateRectangle(
+                spec=(b, 2, xvisitor_dim - b, yvisitor_dim - b),
+                linewidth=0,
+                fillcolor="Green",
+                # text=str(self.topath.n),
+                text=str(self.num),
+                fontsize=xvisitor_dim * 0.7,
+                textcolor="white",
+            )
         return size_x, size_y, an0
 
     def process(self):
@@ -303,6 +332,46 @@ class Visitor(sim.Component):
             if Pipe.ispassive():
                 Pipe.activate()
         self.passivate()
+class Painter(sim.Component):
+    def setup(self,n):
+        self.capacity = painterCapacity
+        self.pictures = PictureinPainter()
+        self.n=n
+        #生成一个随机数，赋给服务器的不同接口,表示处理速度
+        self.solve_time=sim.IntUniform(2,5)()
+    def process(self):
+        while True:
+            if len(self.pictures) >= self.capacity:
+                    for picture in self.pictures:
+                        self.hold(self.solve_time)
+                        picture.leave(self.pictures)
+            self.passivate()
+class PictureinPainter(sim.Queue):
+    pass
+class Picture(sim.Component):
+    def setup(self,img,from_):
+        self.img =img
+        self.painter = Painters[from_]
+    def animation_objects(self, q):#决定了在队列中的动画展现方式
+        size_x = xvisitor_dim
+        size_y = yvisitor_dim
+        b = 0.1 * xvisitor_dim
+        an0 = sim .AnimateImage(
+            image=self.img,
+            x=xvisitor_dim-b,
+            y=yvisitor_dim-b,
+            width=xvisitor_dim,
+            height=yvisitor_dim,
+        )
+        return size_x, size_y, an0
+
+    def process(self):
+        self.enter(self.painter.pictures)
+        # for Pipe in Pipes:
+        #     if Pipe.ispassive():
+        #         Pipe.activate()
+        self.passivate()
+        
 
 
 class VisitorsInPipe(sim.Queue):
@@ -312,6 +381,8 @@ class Server(sim.Component):
         self.capacity = serverCapacity
         self.visitors = VisitorsinServer()
         self.n=n
+        self.painter = Painters[n]
+        
         #生成一个随机数，赋给服务器的不同接口
         self.solve_time=sim.IntUniform(5,10)()
     def process(self):
@@ -320,6 +391,8 @@ class Server(sim.Component):
             if len(self.visitors) >= self.capacity:
                     for visitor in self.visitors:
                         self.hold(self.solve_time)
+                        picture =Picture(img=visitor.img,from_=self.n)
+                        self.painter.activate()
                         visitor.leave(self.visitors)
             self.passivate()
 
@@ -344,7 +417,8 @@ class Pipe(sim.Component):
         
         #设置管道移动的起点和终点
         self.Startx=0
-        self.destx = env.width()-pipeCapacity*xvisitor_dim-serverCapacity*xvisitor_dim
+        offset=pipeCapacity*xvisitor_dim+serverCapacity*xvisitor_dim+SERVER_TO_PAINTER_PATH+painterCapacity*xvisitor_dim
+        self.destx = env.width()-offset
     
     def setStartx(self,x):
         self.Startx=x
@@ -444,11 +518,13 @@ solve_time =5
 dest=100
 load = 50
 pathCapacity = 1
-serverCapacity = 5
+serverCapacity = 3
 pipeCapacity = 1
+painterCapacity = 2
 maxpath =2
 nPipes = maxpath+1
 nServers = maxpath
+nPainters= maxpath
 xvisitor_dim = 40
 while True:
     env = sim.Environment(trace=False)
@@ -460,9 +536,13 @@ while True:
     requests = {}
     paths = [path() for ipath in range(maxpath+1)]
 
+    Painters=[Painter(n=iPainter) for iPainter in range(nPainters)]
+    
     Servers=[Server(n=iServer) for iServer in range(nServers)]
     
     Pipes = [Pipe(n=iPipe) for iPipe in range(nPipes)]
+
+
 
 
     make_video = False
